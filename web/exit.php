88528bbdcbc777af9df972811462c20f4dc38ca2
g0lo8a <c_76@bk.ru>
2020-07-01 20:00:21 +0300

    🔥

--- web/exit.php
+++ web/exit.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
+
 session_start();
 
 session_unset();
+
 header('Location: index.php');
--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
+
 session_start();
+
 include 'header.php';
 
 if (!empty($_SESSION['name'])) {
